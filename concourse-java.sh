6ae7500ade747711b68b1a1f804aa8b6b0ba95a2
Phillip Webb <pwebb@pivotal.io>
2017-08-24 10:41:42 -0700

    Initial commit

---
+++ concourse-java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ============================================================================
+# Concourse Java Support (https://github.com/philwebb/concourse-java-scripts/)
+#
+# Importing scripts should use as follows:
+#
+#     #!/bin/bash
+#     set -e
+#     source <folder>/concourse-java.sh
+#
+# ============================================================================
