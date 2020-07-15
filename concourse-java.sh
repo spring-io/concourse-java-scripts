148beadeadd0d8182f70b41f332d9866bea71843
Stephane Nicoll <snicoll@pivotal.io>
2020-07-15 11:13:28 +0200

    Fix sed in-placed edit option
    Closes gh-11

--- concourse-java.sh
+++ concourse-java.sh
@@ -52,7 +52,7 @@ get_revision_from_pom() {
 set_revision_to_pom() {
 	[[ -n $1 ]] || { echo "missing set_revision_to_pom() argument" >&2; return 1; }
 	grep -q "<revision>.*</revision>" pom.xml || { echo "missing revision tag" >&2; return 1; }
-	sed -i "" -e "s|<revision>.*</revision>|<revision>${1}</revision>|" pom.xml > /dev/null
+	sed --in-place -e "s|<revision>.*</revision>|<revision>${1}</revision>|" pom.xml > /dev/null
 }
 
 # Get the next milestone release for the given number by inspecting current tags
